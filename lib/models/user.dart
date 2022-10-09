bcffa73601795d4e524b9312907c7148c1a0358f
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-09 05:22:53 -0500

    updated user model to get data from map

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -6,7 +6,7 @@ class User {
     host:"heart-registration-app.vercel.app",
     path: "/api/v1/"
   );
-  String _id = "";
+  String id = "";
   String fname = "";
   String lname = "";
   String email = "";
@@ -14,10 +14,17 @@ class User {
   bool isSuperAdmin = false;
 
   Future<Map> login(String email, String password) async {
-    Uri LOGINURI = Uri.http("192.168.8.191:3000","/api/v1/authenticate");
+    Uri LOGINURI = Uri.https("heart-registration-app.vercel.app","/api/v1/authenticate");
     http.Response userData = await http.post(LOGINURI, body:{"email":email, "password":password});
     Map decodedData = jsonDecode(userData.body);
     return decodedData;
+  }
+  void fromMap(Map userData){
+    this.id = userData["_id"];
+    this.fname = userData["fname"];
+    this.lname = userData["lname"];
+    this.email = userData["email"];
+    // return user;
   }
 
 
